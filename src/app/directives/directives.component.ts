15330c401043fc1bac61b4b2b4c55507b3f3490c
Marvin Diaz <marvindaviddiaz@hotmail.com>
2017-02-07 17:59:08 -0600

    ngSwitch

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -19,8 +19,9 @@ import { HighlightDirective } from './highlight.directive'
 export class DirectivesComponent implements OnInit {
   
   private switch = true;
-  
   private items = [1,2,3,4,5];
+  private value = 100;
+  
   
   onSwitch(){
     this.switch = !this.switch;
